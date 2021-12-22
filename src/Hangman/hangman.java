4e7a71273e9f6cf1855cf67141c97c88b0585f25
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-22 12:43:43 +0200

    Hangman 5-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,17 +1,17 @@
 package Hangman;
 import  java.util.Scanner;
-
 import static java.lang.Math.random;
-
 public class hangman {
     public static void main (String[] arg){
         System.out.println("The game will be available soon");
-        String word;
+        char letter;
         String correct_word;
         correct_word = "java";
         Scanner in = new Scanner(System.in);
         int min;
         int max;
+        int letters;
+        int num = 1;
         min=1;
         max=4;
         int cloose_word = min +(int)(random()*(max-min+1));
@@ -27,21 +27,36 @@ public class hangman {
         else if (cloose_word == 4){
             correct_word = "kotlin";
         }
-        for (int i = 0; i < correct_word.length(); i++){
-            if (i<2){
-                System.out.print(correct_word.charAt(i));
+        letters = correct_word.length();
+        char[] word = new char[letters];
+
+        for (int i = 0; i < correct_word.length(); i++) {
+            word[i] = '-';
+        }
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Enter letter:");
+            letter = in.next().charAt(0);
+            for (int o = 0; o < letters; o++){
+                if (letter == correct_word.charAt(o)) {
+                    word[o] = letter;
+                }
+            }
+            System.out.println(word);
+            for (int p = 0; p < letters; p++) {
+                if (word[p] == correct_word.charAt(p)) {
+                    num++;
+                }
+            }
+            if (num == letters) {
+                System.out.println("YOU SURVIVED");
+                System.exit(0);
             }
             else {
-                System.out.println("-");
+                num = 0;
             }
         }
-        word = in.nextLine();
 
-        if (correct_word.equals(word)){
-            System.out.println("YOU SURVIED");
-        }
-        else {
             System.out.println("YOU LOST");
         }
     }
-}
+
