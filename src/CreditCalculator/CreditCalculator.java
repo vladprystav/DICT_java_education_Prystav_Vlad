f530eaaae7b3594da54a7a203ca8bfa0231937b8
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-22 17:35:44 +0200

    CreditCalculator 2 stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -1,11 +1,37 @@
 package CreditCalculator;
+import java.util.Scanner;
+import static java.lang.Math.*;
 
 public class CreditCalculator {
     public static void main (String[] args){
-        System.out.println("Loan principal: 1000\n"+
-                "Month 1: repaid 250\n " +
-                "Month 2: repaid 250\n " +
-                "Month 3: repaid 250\n " +
-                "The loan has been repain! \n");
+        double principal;
+        double months;
+        double monthly_payments;
+        char parametric;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your principal: ");
+        principal = in.nextInt();
+        System.out.print("Enter your parametric (m/n): ");
+        parametric = in.next().charAt(0);
+        if (parametric == 'm') {
+            System.out.print("Enter the monthly payment:");
+            monthly_payments = in.nextInt();
+            months = principal / monthly_payments;
+            months = ceil(months);
+            System.out.println("Your must paying for" + months + "months");
+        }else if (parametric == 'p') {
+            System.out.print("Enter months");
+            months = in.nextInt();
+            monthly_payments = principal / months;
+            monthly_payments = ceil(monthly_payments);
+
+            System.out.print("Your monthy paymens = " + monthly_payments);
+            if (principal - (monthly_payments * months) < 0) {
+                monthly_payments = principal - (monthly_payments * (months - 1));
+                System.out.println(" and last payment =" + monthly_payments);
+            }
+        }else {
+            System.out.println("ERROR");
+        }
     }
 }
