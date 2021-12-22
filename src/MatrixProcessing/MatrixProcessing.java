7d560f5cd269f929a2930e61d2c0637c7f10038d
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-22 19:38:46 +0200

    MatrixProcessing 2 stage

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -9,23 +9,24 @@ public class MatrixProcessing {
         int[] size = new int[2];
         size[0] = in.nextInt();
         size[1] = in.nextInt();
-        System.out.println("Введите первую матрицу:");
+        System.out.println("Введите матрицу:");
         int[][] matriza = new int[size[0]][size[1]];
         for (int q = 0; q < size[0]; q++) {
             for (int w = 0; w < size[1]; w++) {
                 matriza[q][w] = in.nextInt()
             }
         }
-        System.out.println("Введите вторую матрицу:");
+        System.out.println("Введите константу:");
+        c = in.nextInt();
         int[][] matriza2 = new int[size[0]][size[1]];
         for (int q = 0; q < size[0]; q++) {
             for (int w = 0; w < size[1]; w++) {
-                matriza2[q][w] = in.nextInt();
+                matriza2[q][w] = matriza[q][w]*c;
             }
         }
         for (int q = 0; q < size[0]; q++) {
             for (int w = 0; w < size[1]; w++) {
-                System.out.print("|" + matriza3[q][w] + "|");
+                System.out.print("|" + matriza2[q][w] + "|");
             }
             System.out.println();
         }
