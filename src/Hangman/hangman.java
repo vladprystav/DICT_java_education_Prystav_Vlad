f0fefd27134f6207cbaf8e7fb0a7c49b1fbf6b91
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-22 13:11:12 +0200

    Hangman 7-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -14,32 +14,42 @@ public class hangman {
         int num = 1;
         min=1;
         max=4;
-        int cloose_word = min +(int)(random()*(max-min+1));
-        if (cloose_word==1){
+        int choose_word = min +(int)(random()*(max-min+1));
+        if (choose_word==1){
             correct_word = "python";
         }
-        else if (cloose_word == 2){
+        else if (choose_word == 2){
             correct_word = "java";
         }
-        else if (cloose_word == 3){
+        else if (choose_word == 3){
             correct_word = "javascript";
         }
-        else if (cloose_word == 4){
+        else if (choose_word == 4){
             correct_word = "kotlin";
         }
         letters = correct_word.length();
         char[] word = new char[letters];
         char[] use_letters =new char[50];
+        char[] bad_letters = {'Q','W','E','R','T','Y','U','I','O','P','A','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
         int cycle;
         int numb = 0;
         int numb_for_letter = 0;
+        boolean cont = true;
         cycle = 0;
         for (int i = 0; i < correct_word.length(); i++) {
             word[i] = '-';
         }
         for (int i = 0; i < 8; i++) {
-            System.out.println("Enter letter:");
-            letter = in.next().charAt(0);
+            do{
+                cont = true;
+                System.out.println("Enter letter:");
+                letter = in.next().charAt(0);
+                for (int k=0; k<bad_letters.length; k++){
+                    if (letter == bad_letters[k]){
+                        cont = false;
+                    }
+                }
+            } while (cont == false);
             for (int o = 0; o < letters; o++){
                 if (letter == correct_word.charAt(o)) {
                     word[o] = letter;
@@ -53,7 +63,8 @@ public class hangman {
                 }
             }
             if (numb > 0) {
-                System.out.println("No improvements");
+                System.out.println("You've already quessed this letter");
+                i --;
             }
             else if (numb_for_letter == 1) {
                 i--;
