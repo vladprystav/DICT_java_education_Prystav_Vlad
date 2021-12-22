04180951a1d586b466c2f63edacb146499965d48
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-22 17:30:27 +0200

    CreditCalculator 1 stage

---
+++ src/CreditCalculator/CreditCalculator.java
@@ -0,0 +1,11 @@
+package CreditCalculator;
+
+public class CreditCalculator {
+    public static void main (String[] args){
+        System.out.println("Loan principal: 1000\n"+
+                "Month 1: repaid 250\n " +
+                "Month 2: repaid 250\n " +
+                "Month 3: repaid 250\n " +
+                "The loan has been repain! \n");
+    }
+}
