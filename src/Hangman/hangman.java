0a9399af678d959288ee79a670c537dac9852fe5
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-22 12:13:18 +0200

    Hangman 3-rd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,6 +1,8 @@
 package Hangman;
 import  java.util.Scanner;
 
+import static java.lang.Math.random;
+
 public class hangman {
     public static void main (String[] arg){
         System.out.println("The game will be available soon");
@@ -8,6 +10,23 @@ public class hangman {
         String correct_word;
         correct_word = "java";
         Scanner in = new Scanner(System.in);
+        int min;
+        int max;
+        min=1;
+        max=4;
+        int cloose_word = min +(int)(random()*(max-min+1));
+        if (cloose_word==1){
+            correct_word = "python";
+        }
+        else if (cloose_word == 2){
+            correct_word = "java";
+        }
+        else if (cloose_word == 3){
+            correct_word = "javascript";
+        }
+        else if (cloose_word == 4){
+            correct_word = "kotlin";
+        }
         word = in.nextLine();
 
         if (correct_word.equals(word)){
