45f8593921317b12509e5277c4dee0bb2fe069e5
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-23 09:43:12 +0200

    CoffeeMachine1 6 stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,6 +1,5 @@
 package CoffeeMachine;
 import java.util.Scanner;
-
 public class CoffeeMachine {
     public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
@@ -9,6 +8,7 @@ public class CoffeeMachine {
         int milk1, milk2;
         int coffee_beans1, coffee_beans2;
         int money, tap2;
+        int back;
         String tap;
         cups2=0;
         water1 = 400;
@@ -17,19 +17,14 @@ public class CoffeeMachine {
         cups1 = 9;
         money = 550;
         do {
-            System.out.println("The coffee machine has;\n" +
-                    water1 + "of water\n" +
-                    milk1 + "of milk\n" +
-                    coffee_beans1 + "of coffee beans\n" +
-                    cups1 + "of disposable cups\n" +
-                    money + "of money\n");
             System.out.println("What did you do? buy/fill/remaining/exit");
             tap = in.nextLine();
-            if (tap.equals("fill")) {
+            back = CoffeeMachine.take(tap);
+            if (back == 2) {
                 System.out.println("Water +:");
                 water2 = in.nextInt();
-                water1 += water2;
-                System.out.println("Milk +:");
+                water1 += water2; //
+                System.out.println("Milk +: ");
                 milk2 = in.nextInt();
                 milk1 += milk2;
                 System.out.println("Coffee beans +:");
@@ -39,45 +34,69 @@ public class CoffeeMachine {
                 cups2 = in.nextInt();
                 cups1 += cups2;
             }
-            if (tap.equals("take")) {
+            if (back == 3) {
                 money = 0;
             }
-            if (tap.equals("buy")) {
+            if (back == 1) {
                 System.out.println("1 - Espresso, 2 - Latte, 3 - Cappuccino");
                 tap2 = in.nextInt();
-                if (tap2 == 1) {
-                    water1 -= 250;
-                    milk1 -= 0;
-                    coffee_beans1 -= 16;
-                    money += 4;
+                if (tap2 == 1){
+                    water1 -=250;
+                    milk1 -=0;
+                    coffee_beans1 -=16;
+                    money +=4;
                     cups1--;
                 }
-                if (tap2 == 2) {
-                    water1 -= 350;
-                    milk1 -= 75;
-                    coffee_beans1 -= 20;
-                    money += 7;
+                if (tap2 == 2){
+                    water1 -=350;
+                    milk1 -=75;
+                    coffee_beans1 -=20;
+                    money +=7;
                     cups1--;
                 }
-                if (tap2 == 3) {
-                    water1 -= 200;
-                    milk1 -= 100;
-                    coffee_beans1 -= 12;
-                    money += 6;
+                if (tap2 == 3){
+                    water1 -=200;
+                    milk1 -=100;
+                    coffee_beans1 -=12;
+                    money +=6;
                     cups1--;
                 }
             }
-            if ( tap.equals("remaining")){
-                System.out.println("The coffee machine has\n"+
-                        water1+"of water\n"+
-                        milk1+"of milk\n"+
-                        coffee_beans1+"of coffee beans\n"+
-                        cups1+"of disposable cups\n"+
-                        money+"of money\n");
+            if ( back == 4){
+                System.out.println("The coffee machine has:\n"+
+                        water1 + "of water\n" +
+                        milk1 + "of milk\n" +
+                        coffee_beans1 + "of coffee beans\n" +
+                        cups1 + "of disposable cups\n" +
+                        money + "of money\n");
             }
-            if (tap.equals("exit")){
+            if (back == 5){
                 System.exit( 0);
             }
         } while (true);
+
+    }
+}
+class CoffeeMachine {
+    public static int take(String tap) {
+        int take;
+        take = 0;
+        if (tap.equals("buy")){
+            take = 1;
+        }
+        if (tap.equals("fill")){
+            take = 2;
+        }
+        if (tap.equals("take")){
+            take = 3;
+        }
+        if (tap.equals("remaining")){
+            take = 4;
+        }
+        if (tap.equals("exit")){
+            take = 5;
+        }
+
+        return take;
     }
 }
