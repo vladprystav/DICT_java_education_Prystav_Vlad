00f0b8ab2313baf362765c6d8d5d37b1e44e395c
Prystav Vlad <v.v.prystav@student.khai.edu>
2021-12-23 11:49:37 +0200

    TicTacToe2 1 stage

---
+++ src/TicTacToe2/TicTacToe2.java
@@ -0,0 +1,9 @@
+package TicTacToe2;
+
+public class TicTacToe2 {
+    public static void main (String[] args){
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O\n");
+    }
+}
